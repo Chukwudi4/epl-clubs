0848237b9a9968a97da3fa042f5550ea616662fb
Ezichi Chukwudi <chukwudiezichi@gmail.com>
2020-02-03 22:32:01 +0100

    init proj

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Clubs } from './src/screens/clubs';
+
+export default function App() {
+  return (
+    <Clubs/>
+  );
+}
