376ec1a2d5f03f462b6568ce119bf8e6162a1ed5
Ezichi Chukwudi <chukwudiezichi@gmail.com>
2020-02-04 09:56:57 +0100

    add consts

---
+++ src/config/constants.js
@@ -0,0 +1,7 @@
+export const url = "https://api.football-data.org/v2/"
+
+export const headers={
+    "X-Auth-Token": "4111b15344af43e2a3356299504a248d"
+}
+
+export const colors = ['#111e6c', '#1d2951', '#003152', '#000080']
