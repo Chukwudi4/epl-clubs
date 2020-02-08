3fffef2dd719b26709790546bccdccd49ef6be25
Ezichi Chukwudi <chukwudiezichi@gmail.com>
2020-02-08 19:16:11 +0100

    added search feature to clubs

--- src/screens/clubs.js
+++ src/screens/clubs.js
@@ -4,9 +4,12 @@ import {widthPercentageToDP as w} from 'react-native-responsive-screen'
 import { fetchClubs } from '../config/databank'
 import { colors } from '../config/constants'
 import { useNavigation } from 'react-navigation-hooks'
+import { TextInput } from 'react-native'
 export function Clubs(props) {
 
     const [clubList, setClubList] = useState([])
+    const [result, updateResult]  = useState([])
+    const [term, updateTerm] = useState('')
     const {navigate} = useNavigation()
     let a = 1
  
@@ -19,8 +22,44 @@ export function Clubs(props) {
         getClubList()
     }, [a])
 
+    function search() {
+        if(term.length > 1) {
+            let newResult = clubList.filter((club)=>{
+                //console.warn(club.name)
+                return club.name.indexOf(term) != -1
+            })
+
+            updateResult(newResult)
+        }
+    }
+
     return (
     <View style={styles.view} >
+        <TextInput 
+            style={styles.input} 
+            placeholder="Search Clubs" 
+            numberOfLines={10}
+            onChangeText={text=> {
+                updateTerm(text)
+                search()
+            }}
+            placeholderTextColor="#fff" />
+        {
+            term.length>3?
+            <FlatList 
+            data={result}
+            keyExtractor={item=> item.name}
+            renderItem={({item, index})=> 
+                <TouchableOpacity 
+                    activeOpacity={0.9} 
+                    style={[styles.flatItem, {backgroundColor: colors[index% colors.length]}]}
+                    onPress={()=> navigate('ClubDetail', {color:colors[index% colors.length], clubDetails: JSON.stringify(item)})}
+                    >
+                    <Text style={styles.clubName} >{item.name}</Text>
+                </TouchableOpacity>
+            }
+        />        
+            :
         <FlatList 
             data={clubList}
             keyExtractor={item=> item.name}
@@ -33,7 +72,7 @@ export function Clubs(props) {
                     <Text style={styles.clubName} >{item.name}</Text>
                 </TouchableOpacity>
             }
-        />
+        />}
     </View>
     )
 }
@@ -71,5 +110,13 @@ const styles = StyleSheet.create({
     },
     view:{
         flex: 1,
+    },
+    input: {
+        width: w(100),
+        height: w(10),
+        fontSize: w(4.5),
+        backgroundColor: colors[0],
+        color: "#fff",
+        paddingHorizontal: w(2)
     }
 })
