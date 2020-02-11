4ec4d88e6c81ad937ceee609c05bc46bf3418ffb
Ezichi Chukwudi <chukwudiezichi@gmail.com>
2020-02-11 23:07:02 +0100

    fetch fixtures for a particular month

--- src/config/databank.js
+++ src/config/databank.js
@@ -14,8 +14,10 @@ export async function fetchClubs(compId){
 export async function fetchFixtures(teamId){
     
     try {
-
-        let fixture = await fetch(`${url}teams/${teamId}/matches?dateFrom=2020-02-03&dateTo=2020-02-30`,{ headers: headers })    
+        let year = new Date().getFullYear()
+        let month = new Date().getMonth()
+        let daysInMonth = new Date(year, month, 0).getDate()
+        let fixture = await fetch(`${url}teams/${teamId}/matches?dateFrom=2020-02-01&dateTo=2020-02-${daysInMonth}`,{ headers: headers })    
         let fixtureList = await fixture.json()
         //console.warn(fixtureList.matches)
         return fixtureList.matches
